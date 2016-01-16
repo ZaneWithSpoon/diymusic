78e67eb39494f87bb7fe15ff4da68ec79ae59320
Zane <zane.witherspoon@gmail.com>
2016-01-15 16:57:30 -0800

    attempting midi.js imports

--- components/main.js
+++ components/main.js
@@ -20,6 +20,31 @@ const store = createStoreWithMiddleware(diymApp)
 //main.js
 view Main {
 
+  on.mount(() => {
+
+    // console.log('onMount')
+
+    // MIDI.loadPlugin({
+    //   soundfontUrl: "./soundfont/",
+    //   instrument: "acoustic_grand_piano",
+    //   onprogress: function(state, progress) {
+    //     console.log('progress')
+    //     console.log(state, progress)
+    //   },
+    //   onsuccess: function() {
+    //     console.log('success')
+    //     var delay = 0; // play one note every quarter second
+    //     var note = 50; // the MIDI note
+    //     var velocity = 127; // how hard the note hits
+    //     // play the note
+    //     MIDI.setVolume(0, 127);
+    //     MIDI.noteOn(0, note, velocity, delay);
+    //     MIDI.noteOff(0, note, delay + 0.75);
+    //   }
+    // })
+
+  })
+
   let id = store.dispatch(addBeatHypermeasure())
   let bpm = 120
   let speed = 60000/bpm
